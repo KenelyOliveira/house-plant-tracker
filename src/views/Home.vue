cb88f9c9b3573a3b484670c2d00c2cf624f5d783
KenelyOliveira <kenely.oliveira_ges.omnio.com>
2022-02-05 10:18:14 +0100

    add api call

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <br />
+    <Header />
+    <br />
+    <br />
+    <br />
+    <div>
+      <router-link to="/" tag="button" class="btn btn-primary active">Search</router-link>
+    </div>
+    <div>
+      <router-link to="/new-plant" tag="button" class="btn btn-success active">Add new plant</router-link>
+    </div>
+    <div>
+      <button class="btn btn-success active">Add progress</button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import Header from '@/components/Header.vue';
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    Header
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+
+
+}
 </script>
+
+<style scoped>
+.btn {
+  width: 300px;
+}
+div {
+  padding-bottom: 20px;
+}
+</style>
